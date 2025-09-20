f8a52c94d13bb8238b4d896c0a78c5c1f1f1efff
FelixBatista <felixb.neto@gmail.com>
2025-09-20 16:59:45 +0200

    removing old blog folder

--- blog.dogallergycheck.com/htdocs/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Redirect from blog subdomain to main domain /blog
-$redirect_url = 'https://dogallergycheck.com/blog' . $_SERVER['REQUEST_URI'];
-header('Location: ' . $redirect_url, true, 301);
-exit();
-?>
